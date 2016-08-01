c1765fbdad0ebb30b877e4a80d6234adec037d34
damian <damian.ciancio@yahoo.com.ar>
2016-08-01 19:56:50 -0300

    Agregado de JLocalidades

---
+++ src/desktop/JLocalidades.java
@@ -0,0 +1,33 @@
+package desktop;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+
+public class JLocalidades extends JInternalFrame {
+
+	/**
+	 * Launch the application.
+	 
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JLocalidades frame = new JLocalidades();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}*/
+
+	/**
+	 * Create the frame.
+	 */
+	public JLocalidades() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
