d4a671b5e78bdf74ae57f310cd8a2dd7feffd445
Damian <damian.ciancio@yahoo.com.ar>
2016-08-29 01:35:26 -0300

    d

--- src/business/logic/ReporteReclamos.java
+++ src/business/logic/ReporteReclamos.java
@@ -13,11 +13,18 @@ import net.sf.jasperreports.engine.util.JRLoader;
 public class ReporteReclamos {
 	public void reportearTodos() throws JRException, Exception
 	{
-		JasperReport reporte = (JasperReport) JRLoader.loadObject("D:/Software/WORKSPACE JAVA CON GIT/Medidores/GetAllReclamos.jasper");
-		JasperPrint jasperPrint = JasperFillManager.fillReport(reporte, null, Conexion.obtenerConexion());
-		JRExporter exporter = new JRPdfExporter();
-		exporter.setParameter(JRExporterParameter.JASPER_PRINT,jasperPrint); 
-		exporter.setParameter(JRExporterParameter.OUTPUT_FILE,new java.io.File("reportePDF.pdf"));
-		exporter.exportReport();
+		try {
+			JasperReport reporte = (JasperReport) JRLoader.loadObject("D:/Software/WORKSPACE JAVA CON GIT/Medidores/GetAllReclamos.jasper");
+			JasperPrint jasperPrint = JasperFillManager.fillReport(reporte, null, Conexion.obtenerConexion());
+			JRExporter exporter = new JRPdfExporter();
+			exporter.setParameter(JRExporterParameter.JASPER_PRINT,jasperPrint); 
+			exporter.setParameter(JRExporterParameter.OUTPUT_FILE,new java.io.File("reportePDF.pdf"));
+			exporter.exportReport();
+		} catch (JRException jre) {
+			throw new Exception ("Error al generar el reporte", jre);
+		}
+		catch(Exception e){
+			throw new Exception("Error al generar el reporte", e);
+		}
 	}
 }
