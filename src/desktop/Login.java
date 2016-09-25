92d17260294fb63a392e07a567c7aa20c6c0f221
Damian <damian.ciancio@yahoo.com.ar>
2016-09-25 19:10:17 -0300

    p

--- src/desktop/Login.java
+++ src/desktop/Login.java
@@ -31,25 +31,6 @@ public class Login extends JFrame {
 	private Medidores ppal;
 
 
-	/**
-	 * Launch the application.
-	 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Login frame = new Login();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	
-	 * Create the frame.
-	 */
 	public Login(Medidores med) {
 		
 		ppal = med;
